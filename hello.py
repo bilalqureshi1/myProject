0a38f6f335810d84163d71c7ff3e9505993a8242
bilalqureshi1 <61393831+bilalqureshi1@users.noreply.github.com>
2021-09-01 11:59:26 +1000

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-import requests
-from requests.auth import HTTPBasicAuth
-from base64 import b64encode
-## Return the signature token for the admin account
-
-import json
-
-host     = 'https://app.mode.com'
-username = 'c81d4c84b2e6'
